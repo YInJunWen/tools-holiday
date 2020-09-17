2502360f47e9d441a2952bdbb036aa6518e67ae9
yin <yinjwen@gmail.com>
2020-09-17 22:55:21 +0800

    :fire: 移除 图片生成 直接截图就行

--- src/holiday/index.js
+++ src/holiday/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { Row, Col, Select, Form, Input, Button, DatePicker } from 'antd'
 
-import html2canvas from 'html2canvas'
-
 import './index.css'
 
 const { Option, OptGroup } = Select
@@ -13,18 +11,20 @@ class Tool extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      loading: false,
       type: 'A3',
       prop: '个人事务',
       hour: 0.5,
       startTime: '',
       endTime: '',
-      reason: '回家生孩子',
+      reason: '',
       today: this.getTime(new Date()),
-      result: ''
+      userName: '隔壁刘老二',
+      result: null
     }
   }
 
-  handleChange(value) {
+  setType(value) {
     this.setState({
       type: value.split(',')[0],
       prop: value.split(',')[1]
@@ -50,47 +50,45 @@ class Tool extends Component {
     })
   }
 
-  handlerTextareaInput = (e) => {
+  setTextareaInput = (e) => {
     this.setState({
       reason: e.target.value.trim()
     })
   }
 
+  setUserName = (e) => {
+    this.setState({
+      userName: e.target.value.trim()
+    })
+  }
+
   getFormInfo() {
-    const { type, hour, startTime, endTime, reason, prop, today } = this.state
+    const { type, hour, startTime, endTime, reason, prop, userName, today } = this.state
     let result = `
       <p style='text-align:center;'>请假申请</p>
       <p>1. 类型：${type}</p>
       <p>2. 申请人时间：${startTime} 至 ${endTime}, 共${hour}小时</p>
       <p>3. 事由：${reason}</p>
       <p>4. 性质：${prop}</p>
-      <p>5. 申请人：郭风（ 上海乙十生物医疗科技有限公司-技术部）</p>
+      <p>5. 申请人：${userName}（ 上海乙十生物医疗科技有限公司-技术部）</p>
       <p style='text-align:right;'>盼批准，谢谢！</p>
-      <p style='text-align:right;'>员工：郭风</p>
-      <p style='text-align:right;'>申请日期：${today}</p>                              
+      <p style='text-align:right;'>${userName}</p>
+      <p style='text-align:right;'>申请日期：${today}</p>
     `
-    this.setState(
-      {
-        result
-      },
-      () => {
-        html2canvas(document.getElementById('cvs')).then((cvs) => {
-          document.body.appendChild(cvs)
-        })
-      }
-    )
+    this.setState({
+      result
+    })
   }
 
   getTime = (_time) => {
     let time = new Date(_time)
-    var y = this.zeroBu(time.getFullYear()) //年
-    var m = this.zeroBu(time.getMonth() + 1) //月
-    var d = this.zeroBu(time.getDate()) //日
-    var h = this.zeroBu(time.getHours()) //时
-    var mm = this.zeroBu(time.getMinutes()) //分
-    var s = this.zeroBu(time.getSeconds()) //秒
-    var times = y + '-' + m + '-' + d + ' ' + h + ':' + mm + ':' + s
-    return times
+    let y = this.zeroBu(time.getFullYear()) //年
+    let m = this.zeroBu(time.getMonth() + 1) //月
+    let d = this.zeroBu(time.getDate()) //日
+    let h = this.zeroBu(time.getHours()) //时
+    let mm = this.zeroBu(time.getMinutes()) //分
+    let s = this.zeroBu(time.getSeconds()) //秒
+    return y + '-' + m + '-' + d + ' ' + h + ':' + mm + ':' + s
   }
   //补零函数
   zeroBu = (n) => (n < 10 ? '0' + n : n)
@@ -105,8 +103,11 @@ class Tool extends Component {
           layout='horizontal'
           initialValues={{ size: 'default' }}
           size={'default'}>
+          <Form.Item label='尊姓大名'>
+            <Input placeholder='' value={this.state.userName} onChange={(Event) => this.setUserName(Event)} />
+          </Form.Item>
           <Form.Item label='请假类型'>
-            <Select defaultValue='A3' onChange={(Event) => this.handleChange(Event)}>
+            <Select defaultValue={this.state.type} onChange={(Event) => this.setType(Event)}>
               <OptGroup label='A类型 [提前请假]'>
                 <Option value='A1,病假'>A1.病假</Option>
                 <Option value='A2,带薪假期如婚假'>A2.带薪假期如婚假</Option>
@@ -131,10 +132,10 @@ class Tool extends Component {
             />
           </Form.Item>
           <Form.Item label='请假事由'>
-            <TextArea rows={4} onChange={(Event) => this.handlerTextareaInput(Event)} />
+            <TextArea rows={4} onChange={(Event) => this.setTextareaInput(Event)} />
           </Form.Item>
         </Form>
-        <Row>
+        <Row gutter={[16, 16]}>
           <Col span='8'></Col>
           <Col span='10'>
             <Button onClick={(Event) => this.getFormInfo(Event)} type='primary' block>
@@ -143,12 +144,17 @@ class Tool extends Component {
           </Col>
         </Row>
 
-        <Row>
-          <Col span='8'></Col>
-          <Col span='10'>
-            <div id='cvs' dangerouslySetInnerHTML={{ __html: this.state.result }}></div>
-          </Col>
-        </Row>
+        {this.state.result && (
+          <>
+            <Row gutter={[16, 16]}>
+              <Col span='8'></Col>
+              <Col span='10'>
+                <h1>文字版本</h1>
+                <div id='cvs' dangerouslySetInnerHTML={{ __html: this.state.result }}></div>
+              </Col>
+            </Row>
+          </>
+        )}
       </>
     )
   }
